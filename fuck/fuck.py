3a716691d93fec524a2e058ee199f500c6282921
Tony Vallez <Nerdietony@users.noreply.github.com>
2016-10-24 15:04:31 -0400

    Update fuck.py

--- fuck/fuck.py
+++ fuck/fuck.py
@@ -1,4 +1,5 @@
 import discord
+from random import choice as randchoice
 from discord.ext import commands
 
 class Fuck:
@@ -6,12 +7,15 @@ class Fuck:
 
     def __init__(self, bot):
         self.bot = bot
+        self.fuck = ["Fuck you, {}. ~{}"]
 
     @commands.command()
     async def fuck(self, name):
         """Get fuck statements"""
 
-        await self.bot.say("Coming Soon")
+        user = ctx.message.user.name
+        fuck = "+ randchoice(self.fuck) +".format(name, user)
+        await self.bot.say(fuck)
 
 def setup(bot):
     bot.add_cog(Fuck(bot))
