0946df915eb9e285d9dbce7a0e2c6cfe15255b25
Tony Vallez <Nerdietony@users.noreply.github.com>
2016-11-20 18:50:04 -0500

    Update fuck.py

--- fuck/fuck.py
+++ fuck/fuck.py
@@ -9,7 +9,7 @@ class Fuck:
         self.bot = bot
         self.fuck = ["Fuck you, {}. ~{}", "Fucking fuck off, {}. ~{}","Fuck off, {}. ~{}","Fuck this, {}. ~{}", "Fuck that, {}. ~{}","You are a fucking faggot, {}. ~{}","{}, Thou clay-brained guts, thou knotty-pated fool, thou whoreson obscene greasy tallow-catch! ~{}","Oh fuck off, just really fuck off you total dickface. Christ {}, you are fucking thick. ~{}","{}, why don't you go outside and play hide-and-go-fuck-yourself? ~{} ","Hey {}, what a fascinating story, in what chapter do you shut the fuck up?\n\nSincerly,\n{}","What you've just said is one of the most insanely idiotic things I have ever heard, {}. At no point in your rambling, incoherent response were you even close to anything that could be considered a rational thought. Everyone in this room is now dumber for having listened to it. I award you no points :name, and may God have mercy on your soul. ~{}"]
 
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, no_pm=True)
     async def fuk(self, ctx, user : discord.Member=None):
         """Get fuck you statements"""
         
