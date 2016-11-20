040dd47b713ad945110586f8ae8c376247a5727c
Tony Vallez <Nerdietony@users.noreply.github.com>
2016-11-20 16:35:28 -0500

    Added alittle bit of embed + other stuff

--- fuck/fuck.py
+++ fuck/fuck.py
@@ -10,10 +10,19 @@ class Fuck:
         self.fuck = ["Fuck you, {}. ~{}", "Fucking fuck off, {}. ~{}","Fuck off, {}. ~{}","Fuck this, {}. ~{}", "Fuck that, {}. ~{}","You are a fucking faggot, {}. ~{}","{}, Thou clay-brained guts, thou knotty-pated fool, thou whoreson obscene greasy tallow-catch! ~{}","Oh fuck off, just really fuck off you total dickface. Christ {}, you are fucking thick. ~{}","{}, why don't you go outside and play hide-and-go-fuck-yourself? ~{} ","Hey {}, what a fascinating story, in what chapter do you shut the fuck up?\n\nSincerly,\n{}","What you've just said is one of the most insanely idiotic things I have ever heard, {}. At no point in your rambling, incoherent response were you even close to anything that could be considered a rational thought. Everyone in this room is now dumber for having listened to it. I award you no points :name, and may God have mercy on your soul. ~{}"]
 
     @commands.command(pass_context=True)
-    async def fuk(self, ctx, name):
+    async def fuk(self, ctx, user : discord.Member=None):
         """Get fuck you statements"""
-        user = ctx.message.author
-        await self.bot.say("**" + randchoice(self.fuck).format(name, user) + "**")
+        
+        auth = ctx.message.author
+        if not user:
+            data = discord.Embed(colour=auth.colour)
+            data.add_field(name="Error:warning:",value="You have to mention a user, {}".format(auth.mention))
+            await self.bot.say(embed=data)
+        else:
+                fuck = randchoice(self.fuck).format(user.mention, auth.mention)
+                data = discord.Embed(colour=user.colour)
+                data.add_field(name="Fuck You!:middle_finger:",value="{}".format(fuck))
+        await self.bot.say(embed=data)
 
 def setup(bot):
     bot.add_cog(Fuck(bot))
