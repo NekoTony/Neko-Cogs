4ce45eb253f6b1825785e2abbb1e38c096e92cfc
Antonio Vallez <Nerdietony@users.noreply.github.com>
2016-11-26 11:04:12 -0500

    Stats now update every 10 seconds
    I'm lonely

--- botstats/botstats.py
+++ botstats/botstats.py
@@ -2,6 +2,7 @@ from .utils import checks
 from discord.ext import commands
 import discord
 import os
+import asyncio
 
 class BotStats:
     "You can display your bot stats in your game status"
@@ -12,19 +13,21 @@ class BotStats:
     @checks.is_owner()
     @commands.command(pass_context=True, no_pm=True)
     async def botstats(self, ctx):
-        """Display Bot Stats in game status!"""
-        user = ctx.message.author
-        name = self.bot.user.name
-        prefix = ctx.prefix
-        servers = str(len(self.bot.servers))
-        users = str(len(set(self.bot.get_all_members())))
-        message = '{}help | {} servers| {} users'.format(prefix, servers, users)
-        status = list(self.bot.servers)[0].me.status
-        game = discord.Game(name=message)
-        await self.bot.change_presence(status=status, game=game)
-        data = discord.Embed(colour=user.colour)
-        data.add_field(name="Congrats!:sparkles:",value="You have updated your game status based on you bot stats!")
-        await self.bot.say(embed=data)
+        """Display Bot Stats in game status every 10 seconds!"""
+        
+        while True:
+            user = ctx.message.author
+            name = self.bot.user.name
+            prefix = ctx.prefix
+            servers = str(len(self.bot.servers))
+            users = str(len(set(self.bot.get_all_members())))
+            message = '{}help | {} servers | {} users'.format(prefix, servers, users)
+            status = list(self.bot.servers)[0].me.status
+            game = discord.Game(name=message)
+            await self.bot.change_presence(status=status, game=game)
+            await asyncio.sleep(10)
+        else:
+            pass
 
 def setup(bot):
     bot.add_cog(BotStats(bot))
