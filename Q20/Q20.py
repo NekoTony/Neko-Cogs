d173d02f66a5fb14c82aba70bcd38e290d0f6a5d
NerdieTony <Nerdietony@users.noreply.github.com>
2017-03-14 21:23:16 -0400

    Major Update, will push group commands 2morw

--- Q20/Q20.py
+++ Q20/Q20.py
@@ -1,91 +1,93 @@
 import discord
 from discord.ext import commands
 import time
-from random import choice
+from random import choice, sample 
+import os
+from .utils.dataIO import dataIO
 
 class Q20:
     """Play 20 Questions"""
 
     def __init__(self, bot):
         self.bot = bot
+        self.q = "data/q20/settings.json"
+        self.q20 = dataIO.load_json(self.q)
 
     @commands.command(pass_context=True)
-    async def play20(self, ctx, questions=None):
+    async def play(self, ctx, questions=None):
         """Play 20 questions!"""
 
         author = ctx.message.author
         channel = ctx.message.channel
         server = ctx.message.server
 
+        maxq = self.q20["maxq"]
         try:
-            q = 20 if not questions else int(questions)
+            questions = maxq if not questions else int(questions)
         except:
             await self.bot.say("Sorry, but the number you provided must be an actual number.")
             return
         
-        if q > 20:
-            return await self.bot.say("The max amount of questions is 20, sorry.")
+        if questions > self.q20["maxq"]:
+            return await self.bot.say("The max amount of questions is {}, sorry.".format(self.q20["maxq"]))
             return
 
         userz = []
-        await self.bot.say("Want to join? Well say `join` to participate in this round! Total Questions: {}".format(q))
-        timez = time.time() + 60
+        await self.bot.say("Want to join? Well say `join` to participate in this round! Total Questions: {}".format(questions))
+        timez = self.q20["time"]
+        starttime = 0
         while True:
             answer = await self.bot.wait_for_message(channel=channel)
-            picked = 4
+            picked = self.q20["picked"]
             user = answer.author
+            
             if answer.content.lower() == "join" and user.id != author.id:
                 if user.id not in userz:
-                    userz.append(user.id)
+                    userz.append(user)
             
-            if timez <= 0 or time.time() >= timez:
+            if starttime == timez:
                 await self.bot.say("Times up!")
                 break
             
-            if len(userz) >= picked:
+            if len(userz) == picked:
                 await self.bot.say("Max characters has joined!")
                 break
             
-            timez = timez - 1
-            
-        totalusers = len(userz)
-        
-        if totalusers <= 0:
-            await self.bot.say("Sorry but you need atleast 1 person to play.")
-            return
+            time.sleep(1)
+            starttime = starttime + 1
 
-        await self.bot.send_message(author, "Well there bud, you got {} people joining your game. Now let me ask you, what is the answer people need to guess?".format(totalusers))
-        answer = await self.bot.wait_for_message(author=author, timeout=120)
+        userz = sample(userz, len(userz))
+
+        await self.bot.send_message(author, "Well there bud, you got {} people joining your game. Now let me ask you, what is the answer people need to guess?".format(len(userz)))
+        answer = await self.bot.wait_for_message(author=author, timeout=self.q20["time"], check=self.check)
 
         if answer is None:
             await self.bot.send_message(author, "You too too long to reply")
-            await self.bot.say("Didn't provide me an answer")
+            await self.bot.say("{}, didn't provide me with a answer.".format(author))
             return
         else:
             ans = answer.content
             await self.bot.send_message(author, "Perfect! Now tell me, what is the theme for the world you provide?")
 
-        answer2 = await self.bot.wait_for_message(author=author, timeout=120)
+        answer2 = await self.bot.wait_for_message(author=author, timeout=self.q20["time"], check=self.check)
 
         if answer2 is None:
             await self.bot.send_message(author, "You too too long to reply")
-            await self.bot.say("Didn't provide me with a theme")
+            await self.bot.say("{}, didn't provide me with a theme.".format(author))
             return
         else:
             theme = answer2.content
             await self.bot.send_message(author, "Ok, let's get this showboat on a row!")
 
-        await self.bot.say("Hey Guys! Let's begin {} questions! The theme is **{}**".format(q, theme.title()))
+        await self.bot.say("Hey Guys! Let's begin {} questions! The theme is **{}**".format(questions, theme.title()))
         quz = []
         while True:
-            string = "None Yet"
-            if q <= 0:
+            if questions <= 0:
                 await self.bot.say("Questions are done, time for final guesses!")
                 break
             
-            idz = choice(userz)
-            user = server.get_member(idz)
-
+            user = userz[0]
+            print(userz)
             await self.bot.say("{}, would you like to ask a `question` or `guess`.".format(user.mention))
             saying = await self.bot.wait_for_message(author=user, timeout=120)
             
@@ -98,59 +100,101 @@ class Q20:
                 tf = await self.bot.wait_for_message(author=author, timeout=120)
                 if tf is None:
                     await self.bot.say("Sorry, but your time is up!")
+                    userz.append(userz.pop(userz.index(userz[0])))
                 elif tf.content.lower() in ("yes", "y"):
                     add = '{} asked the question "{}" and got the answer was <{}>'.format(user.display_name, question.content, tf.content)
                     quz.append(add)
-                    q = q - 1
+                    userz.append(userz.pop(userz.index(userz[0])))
+                    questions = questions - 1
                 elif tf.content.lower() in ("no", "n"):
                     add = '{} asked the question "{}" and got the answer was <{}>'.format(user.display_name, question.content, tf.content)
                     quz.append(add)
-                    q = q - 1
+                    userz.append(userz.pop(userz.index(userz[0])))
+                    questions = questions - 1
                 elif tf.content.lower() == "idk":
                     add = '{} asked the question "{}" and got the answer was <{}>'.format(user.display_name, question.content, tf.content)
                     quz.append(add)
-                    q = q - 1
+                    userz.append(userz.pop(userz.index(userz[0])))
+                    questions = questions - 1
                 elif tf.content.lower() == "sometimes":
-                    add = '{} asked the question "{}" and got the answer was <{}>'.format(user.display_name, question.content, tf.content)
+                    add = '{} asked the question "{}" and got the answer was "{}"'.format(user.display_name, question.content, tf.content)
                     quz.append(add)
-                    q = q - 1
+                    userz.append(userz.pop(userz.index(userz[0])))
+                    questions = questions - 1
             elif saying.content.lower() == "guess":
                 await self.bot.say("Ok, what is your guess?")
                 guess = await self.bot.wait_for_message(author=user, timeout=120)
 
                 if guess.content.lower() == ans.lower():
-                    await self.bot.say("CONGRATS, {}!! YOU GOT IT RIGHT".format(user.display_name))
+                    await self.bot.say("{}, you got it right!".format(user.display_name))
                     return
                 elif guess is None:
                     await self.bot.say("You took too long to respond.")
+                    userz.append(userz.pop(userz.index(userz[0])))
                 else:
                     await self.bot.say("Sorry, but that guess was incorrect")
                     add = "{} guessed <{}>".format(user.display_name, guess.content)
                     quz.append(add)
-                    q = q - 1
+                    userz.append(userz.pop(userz.index(userz[0])))
+                    questions = questions - 1
 
             else:
                 await self.bot.say("Sorry but you must either choosed guess or question. Your turn has been skipped.")
 
-            string = "\n".join(quz)
-            await self.bot.say("List of Questions:\n```{}```\n{} questions left!".format(string, q))
+            print(userz)
+            
+            try:
+                string = string
+                string = "\n".join(quz)
+                await self.bot.say("List of Questions:\n```{}```\n{} questions left!".format(string, questions))
+            except:
+                pass
 
-        quz = []
-        for x in userz:
-            user = server.get_member(x)
-            await self.bot.say("What is your final guess, {}?".format(user.display_name))
-            zzz = await self.bot.wait_for_message(author=user)
+        maxfg = self.q20["maxfg"]
+        count = 0
+        userz = sample(userz, len(userz))
+        if maxfg != 0:
+            for x in userz:
+                user = server.get_member(x)
+                await self.bot.say("What is your final guess, {}?".format(user.mention))
+                zzz = await self.bot.wait_for_message(author=user)
 
-            if zzz.content.lower() == ans.lower():
-                await self.bot.say("You got it right!")
-                return
-            else:
-                await self.bot.say("Sorry but that was incorrect")
+                if zzz.content.lower() == ans.lower():
+                    await self.bot.say("You got it right!")
+                    return
+                else:
+                    await self.bot.say("Sorry but that was incorrect")
 
-        await self.bot.say("Seems like no one got it! The answer was **{}**".format(ans.title()))
+                count = count + 1
 
+                if count == maxfg:
+                    await self.bot.say("Seems like no one got it! The answer was **{}**".format(ans.title()))
+                    return
+    
+    def check(self, msg):
+        return msg.channel.is_private
 
+def check_folders():
+    if not os.path.exists("data/q20"):
+        print("Creating the Q20 folder, so be patient...")
+        os.makedirs("data/q20")
+        print("Finish!")
 
+def check_files():
+    twentysix = "data/q20/settings.json"
+    json = {
+        "maxq" : 20,
+        "time" : 160,
+        "amount" : 150,
+        "maxfg" : 1,
+        "picked" : 2,
+    }
+
+    if not dataIO.is_valid_json(twentysix):
+        dataIO.save_json(twentysix, json)
+        print("Created json.json!")
 
 def setup(bot):
+    check_folders()
+    check_files()
     bot.add_cog(Q20(bot))
