304e6505e46160ca7c81ea3edd270ecfe78ac4b6
NerdieTony <Nerdietony@users.noreply.github.com>
2017-03-16 14:19:44 -0400

    Update Q20.py

--- Q20/Q20.py
+++ Q20/Q20.py
@@ -262,7 +262,7 @@ class Q20:
         global userz
         author = message.author
         if message.content.lower() == "join":
-            if author not in userz[channel.id]:
+            if author in userz[channel.id]:
                 await self.bot.send_message(channel, "{} has joined!".format(user))
                 
     
