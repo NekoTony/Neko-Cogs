5a405b068b3ef015b4e79f9965fd33a7fbea410d
NerdieTony <Nerdietony@users.noreply.github.com>
2017-03-18 18:27:49 -0400

    Update pressf.py

--- pressf/pressf.py
+++ pressf/pressf.py
@@ -3,13 +3,13 @@ from discord.ext import commands
 from .utils.dataIO import dataIO
 import asyncio
 
-messagem = {}
-messager = {}
 class PressF:
     """You can now pay repect to a person"""
 
     def __init__(self, bot):
         self.bot = bot
+        self.messager = {}
+        self.messagem = {}
 
     @commands.command(pass_context=True, no_pm=True)
     async def pressf(self, ctx, user : discord.User=None):
@@ -17,9 +17,7 @@ class PressF:
 
         author = ctx.message.author
         channel = ctx.message.channel
-        global messager
-        global messagem
-        if channel.id in messager or channel.id in messagem:
+        if channel.id in self.messager or channel.id in self.messagem:
             return await self.bot.send_message(channel, "Opps, can only pay respects if i'm not already paying respects in a channel. Wait till i'm done.")
         
         if user is None:
@@ -33,60 +31,56 @@ class PressF:
             msg = "Everyone, let's pay respects to **{}**! Press f reaction on the this message to pay respects.".format(answer)
         
         else:
-            msg = "Everyone, let's pay respects to **{}**! Press f reaction on the this message to pay respects.".format(user.mention)
+            msg = "Everyone, let's pay respects to **{}**! Press f reaction on the this message to pay respects.".format(user.display_name)
 
         message = await self.bot.send_message(channel, msg)
 
         try:
             await self.bot.add_reaction(message, "\U0001f1eb")
-            messager[channel.id] = [author.id]
+            self.messager[channel.id] = []
             react = True
         except:
-            msg = "Everyone, let's pay respects to **{}**! Type `f` to pay respects.".format(answer)
-            await self.bot.edit_message(message, msg)
-            messagem[channel.id] = [author.id]
+            self.messagem[channel.id] = []
             react = False
 
         await asyncio.sleep(120)
         await self.bot.delete_message(message)
         if react:
-            amount = len(messager[channel.id]) - 1
+            amount = len(self.messager[channel.id]) - 1
         else:
-            amount = len(messagem[channel.id]) - 1
+            amount = len(self.messagem[channel.id]) - 1
         
         if user is None:
-            await self.bot.send_message(channel, "**{}** ppl has payed respects to **{}**".format(amount, answer))
+            await self.bot.send_message(channel, "**{}** {} payed respects to **{}**.".format(amount, "person has" if amount == "1" else "people have", answer))
         else:
-            await self.bot.send_message(channel, "**{}** ppl has payed respects to **{}**".format(amount, user.mention))
+            await self.bot.send_message(channel, "**{}** {} payed respects to **{}**.".format(amount, "person has" if amount == "1" else "people have", user.display_name))
         
         if react:
-            del messager[channel.id]
+            del self.messager[channel.id]
         else:
-            del messagem[channel.id]
+            del self.messagem[channel.id]
     
     async def on_reaction_add(self, reaction, user):
         message = reaction.message
         channel = message.channel
-        global messager
         if user.id == self.bot.user.id:
             return
-        if channel.id not in messager:
+        if channel.id not in self.messager:
             return    
-        if user.id not in messager[channel.id]:
+        if user.id not in self.messager[channel.id]:
             if str(reaction.emoji) == "\U0001f1eb": 
-                await self.bot.send_message(channel, "**{}** has payed respects.".format(user.display_name))
-                messager[channel.id].append(user.id)
+                await self.bot.send_message(channel, "**{}** has paid respects.".format(user.display_name))
+                self.messager[channel.id].append(user.id)
 
     async def on_message(self, message):
         channel = message.channel
         user = message.author
-        global messagem
-        if channel.id not in messagem:
+        if channel.id not in self.messagem:
             return    
-        if user.id not in messagem[channel.id]:
+        if user.id not in self.messagem[channel.id]:
             if message.content.lower() == "f":
-                await self.bot.send_message(channel, "**{}** has payed respects.".format(user.display_name))
-                messagem[channel.id].append(user.id)
+                await self.bot.send_message(channel, "**{}** has paid respects.".format(user.display_name))
+                self.messagem[channel.id].append(user.id)
 
 def setup(bot):
     bot.add_cog(PressF(bot))
